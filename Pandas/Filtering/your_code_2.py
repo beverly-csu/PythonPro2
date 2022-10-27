ef2c2c2ba40af812fddfb6511542cf7f7d8a28a4
a4six <beverly_csu@mail.ru>
2022-10-27 21:41:15 +0500

    Update your_code_2.py

--- Pandas/Filtering/your_code_2.py
+++ Pandas/Filtering/your_code_2.py
@@ -7,14 +7,19 @@ print('Самое дешёвое платное приложение стоит'
 
 # Чему равно медианное (median) количество установок (Installs)
 # приложений из категории (Category) "ART_AND_DESIGN"?
-
+median_installs = df[df['Category'] == 'ART_AND_DESIGN']['Installs'].median()
+print('Медианное количество установок приложений из категории "ART_AND_DESIGN":', median_installs)
 
 # На сколько максимальное количество отзывов (Reviews) для бесплатных приложений (Type == 'Free')
 # больше максимального количества отзывов для платных приложений (Type == 'Paid')?
+max_free = df[df['Type'] == 'Free']['Reviews'].max()
+max_paid = df[df['Type'] == 'Paid']['Reviews'].max()
+print('На', max_free - max_paid, 'отзывов')
 
 
 # Каков минимальный размер (Size) приложения для тинейджеров (Content Rating == 'Teen')?
-
+min_size = df[df['Content Rating'] == 'Teen']['Size'].min()
+print('Минимальный размер приложения для тинейджеров =', min_size, 'Мб')
 
 # *К какой категории (Category) относится приложение с самым большим количеством отзывов (Reviews)?
 
