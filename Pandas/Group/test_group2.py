e3c5d7ee6b531a56fc96ecfd77564b7771ae6d35
a4six <beverly_csu@mail.ru>
2022-11-17 19:17:13 +0500

    Update test_group2.py

--- Pandas/Group/test_group2.py
+++ Pandas/Group/test_group2.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 df = pd.read_csv('GoogleApps.csv')
 
-# temp = df.groupby(by = 'Type')['Rating'].agg(['min', 'mean', 'max'])
-# print('Рейтинг приложений:')
-# print(round(temp, 1))
+temp = df.groupby(by = 'Type')['Rating'].agg(['min', 'mean', 'max'])
+print('Рейтинг приложений:')
+print(round(temp, 1))
 
-# temp = df[df['Type'] == 'Paid'].groupby(by = 'Content Rating')['Price'].agg(['min', 'median', 'max'])
-# print(temp)
+temp = df[df['Type'] == 'Paid'].groupby(by = 'Content Rating')['Price'].agg(['min', 'median', 'max'])
+print(temp)
 
 temp = df.pivot_table(index='Content Rating', columns='Category', values='Reviews', aggfunc='max')
 print(temp[['EDUCATION', 'GAME', 'FAMILY']])
