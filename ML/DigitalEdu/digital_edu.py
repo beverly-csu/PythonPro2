adfd69e9ef1ece269ea5fb8c7061e70ebc1b7edc
a4six <beverly_csu@mail.ru>
2022-12-29 21:10:48 +0500

    as

--- ML/DigitalEdu/digital_edu.py
+++ ML/DigitalEdu/digital_edu.py
@@ -1,9 +1,8 @@
-#создай здесь свой индивидуальный проект!
+# создай здесь свой индивидуальный проект!
 import pandas as pd
 
 df = pd.read_csv('train.csv')
-df.info()
-print(df['education_status'].value_counts())
+
 
 def make_ed_status(status):
     if status == 'Undergraduate applicant':
@@ -24,3 +23,60 @@ def make_ed_status(status):
         return 7
     elif status == 'Candidate of Sciences':
         return 8
+
+
+def make_ed_form(form):
+    if form == 'Distance Learning':
+        return 1
+    elif form == 'Part-time':
+        return 2
+    else:
+        return 0
+
+
+def know_english(langs):
+    langs = langs.split(';')
+    if 'English' in langs:
+        return 1
+    else:
+        return 0
+
+
+def check_occ_type(occupation_type):
+    if occupation_type == 'work':
+        return 0
+    else:
+        return 1
+
+
+df['has_mobile'] = df['has_mobile'].apply(int)
+df['followers_count'] = df['followers_count'].apply(int)
+df['education_status'] = df['education_status'].apply(make_ed_status)
+df['education_form'] = df['education_form'].apply(make_ed_form)
+df['relation'] = df['relation'].apply(int)
+df['english'] = df['langs'].apply(know_english)
+df['occupation_type'] = df['occupation_type'].apply(check_occ_type)
+
+df = df.drop(['id', 'city', 'people_main', 'bdate', 'graduation', 'life_main',
+             'langs', 'last_seen', 'occupation_name', 'career_start', 'career_end'], axis=1)
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+x = df.drop('result', axis=1)
+y = df['result']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
+
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+classifer = KNeighborsClassifier(n_neighbors=3)
+classifer.fit(x_train, y_train)
+
+y_pred = classifer.predict(x_test)
+print(accuracy_score(y_test, y_pred) * 100, '%')
