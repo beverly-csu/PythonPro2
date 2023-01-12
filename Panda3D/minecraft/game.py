d7a88bcd549a5697c5f71d18b8f1f7ebb90bd557
a4six <beverly_csu@mail.ru>
2023-01-12 21:29:21 +0500

    end of lesson

--- Panda3D/minecraft/game.py
+++ Panda3D/minecraft/game.py
@@ -1,1 +1,13 @@
 # напиши здесь код основного окна игры
+from direct.showbase.ShowBase import ShowBase
+from mapmanager import MapManager
+
+class Game(ShowBase):
+    def __init__(self):
+        super().__init__()
+        mapmanager = MapManager()
+        base.camLens.setFov(90)
+
+
+game = Game()
+game.run()
--- Panda3D/minecraft/mapmanager.py
+++ Panda3D/minecraft/mapmanager.py
@@ -1,1 +1,19 @@
 # напиши здесь код создания и управления картой
+class MapManager:
+    def __init__(self):
+        self.model = 'block.egg'
+        self.texture = 'block.png'
+        self.color = (0.57, 1, 0.55, 1)
+        self.startNew()
+        self.addBlock((0, 0, 0))
+
+    def startNew(self):
+        self.land = render.attachNewNode('land')
+
+    def addBlock(self, position):
+        self.block = loader.loadModel(self.model)
+        texture = loader.loadTexture(self.texture)
+        self.block.setTexture(texture)
+        self.block.setPos(position)
+        self.block.setColor(self.color)
+        self.block.reparentTo(self.land)
